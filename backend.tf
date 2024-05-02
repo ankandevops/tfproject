f70a507dda05dfe9c0043c6fd916fb47cfa4e5ca
Ankan Biswas <ankandevops@gmail.com>
2024-05-03 02:20:16 +0530

    First changes

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "demo-resources"
-    storage_account_name = "techtutorialswithpiyush"
+    resource_group_name  = "day-7"
+    storage_account_name = "terraformstorage29"
     container_name       = "prod-tfstate"
     key                  = "prod.terraform.tfstate"
   }
